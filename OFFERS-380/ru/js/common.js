79a90e5965520d09d9a2a7141ffec607b6a22674
Darya <kudiyarova.1975@gmail.com>
2021-06-09 13:32:42 +0300

    h

--- OFFERS-380/ru/js/common.js
+++ OFFERS-380/ru/js/common.js
@@ -48,6 +48,6 @@ function accordion(ref$) {
 }
 $("body").on('click', '[href*="#"]', function (e) {
   var fixed_offset = 100;
-  $('html,body').stop().animate({ scrollTop: $(this.hash).offset().top - fixed_offset }, 1000);
+  $('html,body').stop().animate({ scrollTop: $(this.hash).offset().top - fixed_offset }, 600);
   e.preventDefault();
 });
